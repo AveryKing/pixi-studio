e5d231a9c8769b52e52b6ade2e67067318d76060
Avery King <avery.n.king@wmich.edu>
2022-02-15 23:37:17 -0500

    listeners for select tool added

--- src/index.js
+++ src/index.js
@@ -5,22 +5,15 @@ import { Application } from '@pixi/app';
 import { EventSystem } from '@pixi/events';
 import { Renderer } from '@pixi/core';
 import * as bootstrap from 'bootstrap';
-let selectedTool;
-
 const ElementQueries = require('css-element-queries/src/ElementQueries');
+delete Renderer.__plugins.interaction;
 $('#sidebar-left').resizable();
-// attaches to DOMLoadContent and does anything for you
 ElementQueries.listen();
-
-// or if you want to trigger it yourself:
-// 'init' parses all available CSS and attach ResizeSensor to those elements which
-// have rules attached (make sure this is called after 'load' event, because
-// CSS files are not ready when domReady is fired.
 ElementQueries.init();
 
-const tools = []
 
 let selectedSidebarButton;
+let selectedTool;
 document.querySelectorAll('#sidebar-left a').forEach(button => {
     const toolObj = {};
     toolObj.name = button.dataset.type;
@@ -29,20 +22,15 @@ document.querySelectorAll('#sidebar-left a').forEach(button => {
         if(selectedSidebarButton) selectedSidebarButton.classList.remove('selected-sidebar-button');
         selectedSidebarButton = this.closest('.sidebar-button');
        selectedSidebarButton.classList.add('selected-sidebar-button');
+       if(selectedTool === 'select') {
+           toggleSelectMode(true);
+
+       }
     }
 })
 
-///////
+const draw  = () => {}
 
-let shapeBeingDrawn;
-const resizeShape = (shape, params) => {
-
-}
-const draw  = () => {
-
-}
-
-delete Renderer.__plugins.interaction;
 const app = new PIXI.Application({
     antialias:true,
     backgroundColor:0xffffff,
@@ -61,6 +49,26 @@ const getMousePosition = (e) => {
     return {x: x, y: y};
 }
 
+const itemsOnStage = [];
+const nonDrawTools = ['select', 'move'];
+let selectedItem;
+
+const setupItemMethods = (item) => {
+    item.showMouseOver = () => {
+        console.log('over')
+        item.buttonMode = true;
+        item.alpha = 0.7;
+    }
+    item.stopMouseOver = () => {
+        console.log('out')
+        item.buttonMode = false;
+        item.alpha = 1;
+    }
+    item.doSelect = () => {
+        alert('selected')
+
+    }
+}
 const startDrawing = (e,type) => {
     const {x, y} = getMousePosition(e)
     const newGraphic = new PIXI.Graphics();
@@ -79,11 +87,36 @@ const startDrawing = (e,type) => {
     sprite.x = x;
     sprite.y = y;
     sprite.anchor.set(0.5);
+    setupItemMethods(sprite);
     app.stage.addChild(sprite)
+    itemsOnStage.push(sprite);
 }
 
 
+const toggleSelectMode = (enabled) => {
+    const modifyListeners = (item, listenersEnabled) => {
+        if(listenersEnabled) {
+            item.interactive = true;
+            item.addEventListener('mouseover', item.showMouseOver);
+            item.addEventListener('mousedown', item.doSelect);
+            item.addEventListener('mouseout', item.stopMouseOver);
 
+        } else {
+            item.removeEventListener('mouseover', item.showMouseOver);
+            item.removeEventListener('mouseout', item.stopMouseOver);
+            item.removeEventListener('mousedown', item.doSelect);
+        }
+    }
+    if(!enabled) {
+        itemsOnStage.forEach(item => {
+            modifyListeners(item,false);
+        })
+    } else {
+        itemsOnStage.forEach(item => {
+            modifyListeners(item, true);
+        })
+    }
+}
 document.querySelector('#lab-view').appendChild(app.view);
 const addEvt  = () => document.querySelector('canvas').addEventListener('mousemove',(e) => draw(e));
 const rmEvt  = () => document.querySelector('canvas').removeEventListener('mousemove', (e) => draw(e));
@@ -95,7 +128,18 @@ app.view.addEventListener('mousedown', function handleClick(e)
         new bootstrap.Toast('#myToast').show();
     } else {
         addEvt();
-        startDrawing(e,selectedTool);
+        if(!nonDrawTools.includes(selectedTool)) {
+            startDrawing(e,selectedTool);
+            toggleSelectMode(false);
+
+        } else {
+            switch(selectedTool) {
+                case 'select':
+                    break;
+                case 'move':
+                    break;
+            }
+        }
     }
 });
 
